c6e9234a87784692f633f7b623844d0dec714489
Mitchell Chambers <mitchell.chambers@puppet.com>
2023-06-07 10:13:20 +1000

    add class manifests to mod

---
+++ manifests/packages.pp
@@ -0,0 +1,37 @@
+# @summary
+#
+# @param package_list [Hash]
+#   Define a hash of packages and manage attributes
+#   
+# @example Using Puppet resource syntax
+#   class { "delegate_module::packages":
+#     package_list = {
+#       jq => {
+#         ensure => 1.6.0
+#       }
+#     }
+#   }
+#
+# @example Use Hiera APL 
+#   include "delegate_module::packages"
+#
+#   data/common.yaml 
+#   ---
+#   # Specify package_list using hiera hash syntax
+#   delegate_module::packages::package_list:
+#     jq:
+#       ensure: 1.6.0
+#
+class delegate_module::packages (
+  Hash $package_list = {},
+) {
+  $package_list.each | String $package, Hash $package_opts | {
+    package {
+      default:
+        ensure => present,
+        ;
+      $package:
+        * => pick($package_opts,{}),
+    }
+  }
+}
