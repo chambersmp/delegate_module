00efa704c690ce6ccfb302df061405a7f4ef780a
Mitchell Chambers <mitchell.chambers@puppet.com>
2023-06-21 17:52:42 +1000

    add notify resource to files

---
+++ manifests/files.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include delegate_module::files
+class delegate_module::files {
+  notify { 'Test main': }
+}
